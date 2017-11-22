430a79c2782d342c6b2f73ed3e2b267bccaba397
gkaragoz <gkaragoz96@gmail.com>
2017-11-22 14:05:20 +0300

    On Off Light script with Input spaces.

---
+++ 03 Course Materials/Scripting 101/Assets/Scripts/OnOffLight.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnOffLight : MonoBehaviour {
+
+    public Light light;
+
+    private void Awake()
+    {
+        light = GetComponent<Light>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+            OnOff();
+    }
+
+    void OnOff()
+    {
+        light.enabled = !light.enabled;
+    }
+}
