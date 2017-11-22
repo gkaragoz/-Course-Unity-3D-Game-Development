e7bc7d6ed5ba1545abac358e31dbaeed321083e1
gkaragoz <gkaragoz96@gmail.com>
2017-11-22 15:22:17 +0300

    Add GameManager script

---
+++ 03 Course Materials/Scripting 101/Assets/Scripts/GameManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public GameObject [] golds;
+
+	void Start () {
+        golds = GameObject.FindGameObjectsWithTag("Gold");
+	}
+	
+	void Update () {
+		
+	}
+}
