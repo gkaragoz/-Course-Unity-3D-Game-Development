2d458f0467c1c7b201c43a4b44d9ca64521de360
gkaragoz <gkaragoz96@gmail.com>
2017-11-22 14:55:22 +0300

    Create roll a ball project and add PlayerController script via Rigidbody methods.

---
+++ 03 Course Materials/Scripting 101/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed = 5f;
+    public Rigidbody rigidbody;
+
+	void Start () {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+	
+	void FixedUpdate () {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector3 dir = new Vector3(horizontal, 0, vertical);
+        // transform.Translate(speed * dir * Time.deltaTime); non physics method
+    
+        rigidbody.AddForce(dir * speed);
+	}
+}
