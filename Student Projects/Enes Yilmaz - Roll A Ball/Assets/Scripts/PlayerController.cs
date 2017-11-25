3fbf75e32cac65c726c9443054fafe48aac80847
gkaragoz <gkaragoz96@gmail.com>
2017-11-25 12:05:06 +0300

    Add Student Project - Enes Yilmaz

---
+++ Student Projects/Enes Yilmaz - Roll A Ball/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour {
+
+    public Rigidbody rigidbody;
+    public float speed = 10f;
+    public Text scoreText;
+    public Text winText;
+
+    private int score;
+	void Start () {
+        rigidbody = GetComponent<Rigidbody>();
+        score = 0;
+        SetScoreText();
+        winText.text = "";
+	}
+
+
+	// Update is called once per frame
+	void FixedUpdate () {
+        if (Input.GetKeyDown(KeyCode.Space))
+            Jump();
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Move(horizontal, vertical);
+    }
+
+    void Move(float horizontal, float vertical)
+    {
+        Vector3 dir = new Vector3(horizontal, 0, vertical);
+
+        rigidbody.AddForce(dir * speed);
+    }
+    void Jump()
+    {
+        if (transform.localPosition.y == 0.5 || transform.localPosition.y == 1.5 || transform.localPosition.y == 4 || transform.localPosition.y == 5)
+        {
+            Vector3 dir = new Vector3(0, 400f, 0);
+            rigidbody.AddForce(dir);
+        }
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Gold"))
+        {
+            other.gameObject.SetActive(false);
+            score++;
+            SetScoreText();
+        }
+        if (other.gameObject.CompareTag("Key"))
+        {
+            other.gameObject.SetActive(false);
+            Destroy(GameObject.FindWithTag("Door"));
+        }
+    }
+
+    void SetScoreText()
+    {
+        scoreText.text = "Score:" + score;
+        if (score >= 6)
+        {
+            winText.text = "You Win";
+        }
+    }
+}
---
+++ Student Projects/Enes Yilmaz - Roll A Ball/Assets/Scripts/Rotate.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour {
+
+    public float rotateX;
+    public float rotateY;
+    public float rotateZ;
+
+    void Awake()
+    {
+        rotateX = Random.Range(10f, 50f);
+        rotateY = Random.Range(10f, 50f);
+        rotateZ = Random.Range(10f, 50f);
+    }
+
+    void FixedUpdate()
+    {
+        transform.Rotate(new Vector3(rotateX, rotateY, rotateZ) * Time.deltaTime);
+    }
+}
