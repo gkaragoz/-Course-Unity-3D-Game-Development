b926444dafbde02cc2343949c0a82d7f813e45b3
gkaragoz <gkaragoz96@gmail.com>
2017-11-29 17:29:26 +0300

    Create Raycast Example 01 Scene.

---
+++ 04 Course Materials/Roll A Ball/Assets/Scripts/MouseController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseController : MonoBehaviour {
+
+    public ScalableObject scalableObj;
+
+    private RaycastHit _hit;
+
+    void Update()
+    {
+        CheckClick();
+    }
+
+    /// <summary>
+    /// Checks Right or left click events.
+    /// </summary>
+    void CheckClick()
+    {
+        //Mouse left click.
+        if (Input.GetMouseButtonDown(0))
+            Interact();
+        //Right mouse click.
+        else if (Input.GetMouseButtonDown(1))
+            Interact(-1);
+    }
+
+    /// <summary>
+    /// Interact with objects via Raycast. The objects must have at least 1 collider.
+    /// </summary>
+    /// <param name="oppositeScale"></param>
+    void Interact(int oppositeScale = 1)
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out _hit))
+        {
+            scalableObj = _hit.collider.gameObject.GetComponent<ScalableObject>();
+            if (scalableObj != null)
+                scalableObj.ScaleIt(oppositeScale);
+        }
+    }
+}
---
+++ 04 Course Materials/Roll A Ball/Assets/Scripts/ScalableObject.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScalableObject : MonoBehaviour {
+
+    public Vector3 newScale;
+
+    private void Start()
+    {
+        newScale = transform.localScale;
+    }
+
+    /// <summary>
+    /// Change scale of gameObject. The parameter makes bigger or smaller. Default is make it bigger.
+    /// </summary>
+    /// <param name="oppositeScale"></param>
+    public void ScaleIt(int oppositeScale = 1)
+    {
+        Vector3 currentScale = transform.localScale;
+
+        transform.localScale = new Vector3(
+            currentScale.x + newScale.x * 0.5f * oppositeScale, 
+            currentScale.y + newScale.y * 0.5f * oppositeScale, 
+            currentScale.z + newScale.z * 0.5f * oppositeScale);
+
+        if (transform.localScale.magnitude <= 1)
+            transform.localScale = currentScale;
+    }
+
+}
